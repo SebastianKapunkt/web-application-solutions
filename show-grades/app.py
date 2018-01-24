0dab552be6b4b76e98ef454b5e9395c2d4028680
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2018-01-24 21:07:19 +0100

    Extends angular with sending of grades

--- show-grades/app.py
+++ show-grades/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -22,6 +22,33 @@ def get_grades():
         {
             "grade": 100,
             "name": "Berta"
+        }
+    ]
+    # calculating grades , details ignored
+    return jsonify(grades)
+
+
+@app.route("/grades2/", methods=["POST"])
+def set_grades():
+    print(request.form['name'])
+    print(request.form['grade'])
+
+    grades = [
+        {
+            "grade": 95,
+            "name": "Anton"
+        },
+        {
+            "grade": 90,
+            "name": "Caesar"
+        },
+        {
+            "grade": 200,
+            "name": "Berta"
+        },
+        {
+            "grade": request.form['grade'],
+            "name": request.form['name']
         }
     ]
     # calculating grades , details ignored
